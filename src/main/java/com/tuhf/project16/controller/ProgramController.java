0c1d4705a0253cdfc06900acbb2aaad5a72cbd0d
Zhang.Lizhong <3080529100@qq.com>
2024-07-02 15:29:22 +0800

    增加了项目申请和项目模板的接口

---
+++ src/main/java/com/tuhf/project16/controller/ProgramController.java
@@ -0,0 +1,62 @@
+package com.tuhf.project16.controller;
+
+
+import com.tuhf.project16.mapper.ProgramMapper;
+import com.tuhf.project16.model.ProgramApplication;
+import com.tuhf.project16.model.ProgramTemplate;
+import com.tuhf.project16.payload.request.AddProgramApplicationRequest;
+import com.tuhf.project16.payload.request.AddProgramTemplateRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author 30805
+ */
+@RestController
+@RequestMapping("/api/program")
+public class ProgramController {
+
+    @Autowired
+    ProgramMapper programMapper;
+
+    @PostMapping("template/add")
+    @PreAuthorize("hasAnyAuthority('enterprise','carrier','government')")
+    public ResponseEntity<?> addProgramTemplate(@RequestBody AddProgramTemplateRequest addProgramTemplateRequest) {
+        try {
+            programMapper.addProgramTemplate(new ProgramTemplate(addProgramTemplateRequest));
+            return ResponseEntity.ok("Add program template successful!");
+        }catch (Exception e) {
+            return ResponseEntity.ok("Add program template failed!");
+        }
+    }
+
+    @GetMapping("template/query")
+    @PreAuthorize("hasAnyAuthority('enterprise','carrier','government')")
+    public ResponseEntity<?> getTemplateById(@RequestParam(value = "id") Long id) {
+        return ResponseEntity.ok(programMapper.getProgramTemplateById(id));
+    }
+
+    @GetMapping("application/query")
+    @PreAuthorize("hasAnyAuthority('enterprise','carrier','government')")
+    public ResponseEntity<?> getApplicationById(@RequestParam(value = "id",defaultValue = "-1") Long id,
+                                                @RequestParam(value = "sourceId",defaultValue = "-1") Long sourceId,
+                                                @RequestParam(value = "sourceType",defaultValue = "-1") int sourceType,
+                                                @RequestParam(value = "destinationId",defaultValue = "-1") Long destinationId,
+                                                @RequestParam(value = "destinationType",defaultValue = "-1") int destinationType) {
+        if(id!=-1) {
+            return ResponseEntity.ok(programMapper.getProgramApplicationById(id));
+        }else{
+            return ResponseEntity.ok(programMapper.getProgramApplications(sourceId,sourceType,destinationId,destinationType));
+        }
+    }
+
+    @PostMapping("application/add")
+    @PreAuthorize("hasAnyAuthority('enterprise','carrier','government')")
+    public ResponseEntity<?> addApplication(@RequestBody AddProgramApplicationRequest addProgramApplicationRequest){
+        programMapper.addProgramApplication(new ProgramApplication(addProgramApplicationRequest));
+        return ResponseEntity.ok("Add program application successful!");
+    }
+
+}
---
+++ src/main/java/com/tuhf/project16/mapper/ProgramMapper.java
@@ -0,0 +1,33 @@
+package com.tuhf.project16.mapper;
+
+import com.tuhf.project16.model.ProgramApplication;
+import com.tuhf.project16.model.ProgramTemplate;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * @author 30805
+ */
+public interface ProgramMapper {
+
+    void addProgramTemplate(ProgramTemplate programTemplate);
+
+    ProgramTemplate getProgramTemplateById(Long id);
+
+    Collection<ProgramTemplate> getProgramTemplatesByType(int type);
+
+    Collection<ProgramTemplate> getProgramTemplatesByGovernmentId(long governmentId);
+
+    void addProgramApplication(ProgramApplication programApplication);
+
+    ProgramApplication getProgramApplicationById(Long id);
+
+    /**
+        type企业enterprise为0，载体carrier为1，政务government为2
+        id,type不受限制则填写-1
+     */
+    Collection<ProgramApplication> getProgramApplications(long sourceId, long sourceType,
+                                                          long destinationId, long destinationType);
+
+}
